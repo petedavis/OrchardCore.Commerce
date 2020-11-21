c5c97ae1c59de21ef9a38854056352763bc1dba4
Peter Davis <peter.w.davis@gmail.com>
2020-11-21 14:04:39 +1030

    Added initial sample store app

---
+++ src/OrchardCore.Themes/TheSampleStoreTheme/Manafest.cs
@@ -0,0 +1,11 @@
+﻿using OrchardCore.DisplayManagement.Manifest;
+using OrchardCore.Modules.Manifest;
+
+[assembly: Theme(
+    Name = "The Sample Store Theme",
+    Author = ManifestConstants.OrchardCoreTeam,
+    Website = ManifestConstants.OrchardCoreWebsite,
+    Version = ManifestConstants.OrchardCoreVersion,
+    Description = "Sample store theme to demonstrate OrchardCore.Commerce",
+    Tags = new[] { "Commerce" }
+)]
---
+++ src/OrchardCore.Themes/TheSampleStoreTheme/ResourceManifest.cs
@@ -0,0 +1,21 @@
+﻿using OrchardCore.ResourceManagement;
+
+namespace TheSampleStoreTheme
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(IResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            manifest
+                .DefineStyle("TheSampleStoreTheme")
+                .SetUrl("~/TheSampleStoreTheme/css/TheSampleStoreTheme.min.css", "~/TheSampleStoreTheme/css/TheSampleStoreTheme.css")
+                .SetVersion("1.0.0");
+
+            manifest.DefineScript("TheSampleStoreTheme")
+                .SetUrl("~/TheSampleStoreTheme/js/TheSampleStoreTheme.min.js", "~/TheSampleStoreTheme/js/TheSampleStoreTheme.js")
+                .SetVersion("1.0.0");
+        }
+    }
+}
